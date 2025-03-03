75e54aaab54ca3d85421a527c1c919da4f08e60a
sashank <sashanktalakola2@gmail.com>
2025-03-03 18:06:28 -0500

    created basic validator and reloader for config manger

---
+++ backend-core/nginx_config_manager/validator.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+
+class NginxConfigValidator:
+    @staticmethod
+    def validate_nginx_config():
+
+        execution_environment = os.getenv("EXEC_ENV")
+        
+        if execution_environment == "local":
+            result = subprocess.run(
+                ["sudo", "nginx", "-t"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            
+        elif execution_environment == "docker":
+            result = subprocess.run(
+                ["nginx", "-t"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+
+        if result.returncode == 0:
+            print("NGINX configuration is valid.")
+            return True
+        else:
+            print(f"NGINX configuration validation failed: {result.stderr.decode()}")
+            return False
