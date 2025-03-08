94dd41a08e514a1230c56d57addf5a993404087c
sashanktalakola <sashanktalakola2@gmail.com>
2025-03-08 16:29:37 -0500

    updated main script to utilize kafka broker to create new containers

--- backend-core/main.py
+++ backend-core/main.py
@@ -1,16 +1,36 @@
 from nginx_config_manager.manager import NginxConfigManager
+from kafka_consumer import get_consumer
 
 config_manager = NginxConfigManager()
+consumer = get_consumer()
 
-# Testing
-subdomain = "test"
-domain = "csci-b516"
-container_dns = "flask-container"
-container_port = "5000"
+# # Testing
+# subdomain = "test"
+# domain = "csci-b516"
+# container_dns = "flask-container"
+# container_port = "5000"
 
-config_manager.manage_nginx_config(
-    subdomain,
-    domain,
-    container_dns,
-    container_port
-)
+# config_manager.manage_nginx_config(
+#     subdomain,
+#     domain,
+#     container_dns,
+#     container_port
+# )
+
+for message in consumer:
+    data = message.value
+    # print("\nReceived JSON message:")
+    # print(json.dumps(data, indent=2))
+    
+    # Extract fields
+    subdomain = data.get("subdomain")
+    domain = data.get("domain")
+    container_dns = data.get("container_dns")
+    container_port = data.get("container_port")
+
+    config_manager.manage_nginx_config(
+        subdomain,
+        domain,
+        container_dns,
+        container_port
+    )
